0a59fbaa0be93dd77daccbd6f3c37ade40cc85bb
tomekhub <thomas@linux.pl>
2018-03-31 19:50:27 +0200

    added dashboard skeleton and expansion animation to alert button

--- WSB/Controllers/HomeController.swift
+++ WSB/Controllers/HomeController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var expandableView: UIView!
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var weatherView: UIView!
+    @IBOutlet weak var eventView: UIView!
+    @IBOutlet weak var tipView: UIView!
+    @IBOutlet weak var alertArrow: UIImageView!
+    
+    @IBOutlet weak var expandableViewConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //hiding the expandableView
+        self.expandableViewConstraint.constant = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func alertButtonClick(_ sender: Any) {
+        expandAlertButton()
+    }
+    
+    
+    func expandAlertButton() {
+        view.layoutIfNeeded()
+        let height: CGFloat = (self.expandableViewConstraint.constant == 0) ? 150 : 0
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.expandableViewConstraint.constant = height
+            self.alertArrow.transform = self.alertArrow.transform.rotated(by: CGFloat(Double.pi))
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    
 
 }
 
